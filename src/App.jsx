0114674f852f63e35f0db42a4984778014d2b4e0
muradgm <59296531+muradgm@users.noreply.github.com>
2022-05-08 03:30:41 +0200

    new additional changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import generateFakeData, { tableHeader, tableData }from './lib/fake.js'
+import React, {useEffect, useState} from 'react';
+
+import generateFakeData, { tableHeader, tableData } from './lib/fake.js'
 import randomColor, { randomGradient } from './lib/randomColor.js'
 import './base.scss'
 
 export default function App() {
     const data = generateFakeData()
+    const [newData, setNewData] = useState();
     const header = tableHeader(data)
     const body = tableData(data)
+
+    const handleClick = () => { 
+        setNewData(data)
+    }
+
 
     console.log(header);
 
     return (
         <main>
+            <div className="header">
+                <h1>random card generator</h1>
+                <button className="btn" onClick={handleClick}>generate new cards</button>
+            </div>
             <ol>
                 {data.map((item, i) => (
-                    <li key={i} className="card" style={{ background: `${randomGradient()}` }}><p>Hey, </p><p> I'm <strong>{item.name}</strong></p>and i live in <strong>{item.city}</strong>
-                        <p>lets get in touch : </p>{item.telephone}</li>
+                    <li key={i} className="card" style={{ background: `${randomGradient()}` }}
+                    >
+                        <img src={item.avatar} alt="avatar" />
+                        <p style={{fontWeight: 700}}>Hey, </p><p> I'm <strong>{item.name}</strong>,</p><p> I'm from <strong>{item.city}</strong></p>
+                        <span className="line"></span>
+                        <p>lets get in touch : </p><p>{item.telephone}</p></li>
                 ))}
             </ol>
         </main>
--- src/lib/fake.js
+++ src/lib/fake.js
@@ -1,62 +1,88 @@
+const avatars = {
+	avatar: require("./avatars/avatar1.png"),
+	avatar2: require("./avatars/avatar2.png"),
+	avatar3: require("./avatars/avatar3.png"),
+	avatar4: require("./avatars/avatar4.png"),
+	avatar5: require("./avatars/avatar5.png"),
+	avatar6: require("./avatars/avatar6.png"),
+	avatar7: require("./avatars/avatar7.png"),
+	avatar8: require("./avatars/avatar8.png"),
+	avatar9: require("./avatars/avatar9.png"),
+	avatar10: require("./avatars/avatar10.png"),
+	avatar11: require("./avatars/avatar11.png"),
+	avatar12: require("./avatars/avatar12.png"),
+	avatar13: require("./avatars/avatar13.png"),
+	avatar14: require("./avatars/avatar14.png"),
+};
+
 export default function generateFakeData() {
+	const data = [];
+	let randomName = "";
+	let randomCity = "";
+	let randomEmail = "";
+	let randomAvatar = "";
+	let randomTel = "+";
+	let names = ["Michael", "John", "Jay", "Bob", "Ronny", "James"];
+	let last = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Wilson"];
+	let cities = ["Dubai", "London", "New York", "Berlin", "Paris", "Istanbul"];
+	let at = ["@gmail.com", "@hotmail.com", "@yahoo.com", "@hubspot.net"];
+	let code = [491, 971, 441, 331, 901];
+	let randomNum = Math.floor(Math.random() * 900000000) + 100000000;
 
-    const data = []
-    let randomName = ''
-    let randomCity = ''
-    let randomEmail = ''
-    let randomTel = '+'
-    let names = ['Michael', 'John', 'Jay', 'Bob', 'Ronny', 'James'];
-    let last = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Wilson']
-    let cities = ['Dubai', 'London', 'New York', 'Berlin', 'Paris', 'Istanbul']
-    let at = ['@gmail.com', '@hotmail.com', '@yahoo.com', '@hubspot.net']
-    let code = [491, 971, 441, 331, 901]
-    let randomNum = Math.floor(Math.random()*900000000)+100000000
+	for (let i = 1; i < 25; i++) {
+		randomName =
+			names[Math.floor(Math.random() * names.length)] +
+			" " +
+			last[Math.floor(Math.random() * names.length)];
 
-    for (let i = 1; i < 25; i++) {
-        randomName = names[Math.floor(Math.random() * names.length)] +' '+ last[Math.floor(Math.random() * names.length)];
+		randomCity = cities[Math.floor(Math.random() * names.length)];
 
-        randomCity = cities[Math.floor(Math.random() * names.length)];
+		randomEmail =
+			randomName.toLowerCase().charAt(0) +
+			last[Math.floor(Math.random() * names.length)].toLowerCase() +
+			at[Math.floor(Math.random() * at.length)];
 
-        randomEmail = randomName.toLowerCase().charAt(0) + last[Math.floor(Math.random() * names.length)].toLowerCase() +  at[Math.floor(Math.random() * at.length)];
+		randomTel =
+			"+" + code[Math.floor(Math.random() * code.length)] + " " + randomNum;
+		// console.log('random number', );
 
-        randomTel = '+' + code[Math.floor(Math.random() * code.length)] + ' ' + randomNum;
-        // console.log('random number', );
+		randomAvatar =
+			avatars[
+				Object.keys(avatars)[
+					Math.floor(Math.random() * Object.keys(avatars).length)
+				]
+			];
 
-        console.log(randomTel);
-    data.push({
-        id: i,
-        name: randomName,
-        city: randomCity,
-        email: randomEmail,
-        telephone: randomTel
-    })
-    }
-    return data;
+		console.log(randomTel);
+		data.push({
+			id: i,
+			avatar: randomAvatar,
+			name: randomName,
+			city: randomCity,
+			email: randomEmail,
+			telephone: randomTel,
+		});
+	}
+	return data;
 }
 
-let fakeData = generateFakeData()
-console.log(fakeData);
+let fakeData = generateFakeData();
+// console.log(fakeData);
 
 export function tableHeader(str) {
-    let header = Object.keys(str[0])
-    return header.map(key => {
-        return key.toUpperCase()
-    })
+	let header = Object.keys(str[0]);
+	return header.map((key) => {
+		return key.toUpperCase();
+	});
 }
 // console.log(tableHeader(fakeData));
 
-
 export function tableData(data) {
-    return data.map((value,i) => {
-        const { id, name, city, email } = value;
-        // console.log(value);
+	return data.map((value, i) => {
+		const { id, name, city, email } = value;
+		// console.log(value);
 
-        return[
-            id,
-            name,
-            city,
-            email
-        ]
-    })
+		return [id, name, city, email];
+	});
 }
 console.log(tableData(fakeData));
